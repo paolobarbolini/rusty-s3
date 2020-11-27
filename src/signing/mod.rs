55315415eb18c515e9bc722a3da67ccaefb2f389
Paolo Barbolini <paolo@paolo565.org>
2020-11-27 18:00:07 +0100

    signing: add tests for sign
    Uses the example from: https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-query-string-auth.html#query-string-auth-v4-signing-example
    
    Signed-off-by: Paolo Barbolini <paolo@paolo565.org>
    Co-authored-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/signing/mod.rs
+++ src/signing/mod.rs
@@ -33,13 +33,13 @@ pub fn sign(
         &url,
         vec![
             ("X-Amz-Algorithm", "AWS4-HMAC-SHA256"),
-            ("X-Amz-Credential", &credential),
+            ("X-Amz-Credential", &credential_),
             ("X-Amz-Date", &date_str),
             ("X-Amz-Expires", &expires_seconds.to_string()),
-            ("X-Amz-SignedHeader", signed_headers_str),
+            ("X-Amz-SignedHeaders", signed_headers_str),
         ]
         .into_iter(),
-        vec![("host", "localhost")].into_iter(),
+        vec![("host", url.host_str().unwrap())].into_iter(),
         vec!["host"].into_iter(),
     );
 
@@ -52,4 +52,35 @@ pub fn sign(
     let signature = signature::signature(date, secret, region, &signed_string);
 
     format!("{}&X-Amz-Signature={}", url_query, signature)
+}
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+    use time::PrimitiveDateTime;
+
+    use super::*;
+
+    #[test]
+    fn aws_example() {
+        let date = PrimitiveDateTime::parse(
+            "Fri, 24 May 2013 00:00:00 GMT",
+            "%a, %d %b %Y %-H:%M:%S GMT",
+        )
+        .unwrap();
+        let method = "GET";
+        let url = "https://examplebucket.s3.amazonaws.com/test.txt"
+            .parse()
+            .unwrap();
+        let key = "AKIAIOSFODNN7EXAMPLE";
+        let secret = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+        let region = "us-east-1";
+        let expires_seconds = 86400;
+
+        let expected = "https://examplebucket.s3.amazonaws.com/test.txt?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIOSFODNN7EXAMPLE%2F20130524%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20130524T000000Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=aeeed9bbccd4d02ee5c0109b86d86835f995330da4c265957d157751f604d404";
+
+        let got = sign(&date, method, &url, key, secret, region, expires_seconds);
+
+        assert_eq!(expected, got);
+    }
 }
