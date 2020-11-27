415919b16e5a878b1ac737fc6d4dc8cf3be726a9
Paolo Barbolini <paolo@paolo565.org>
2020-11-27 17:50:39 +0100

    signing: add tests for signature
    Uses the example from: https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-query-string-auth.html#query-string-auth-v4-signing-example
    
    Signed-off-by: Paolo Barbolini <paolo@paolo565.org>
    Co-authored-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/signing/signature.rs
+++ src/signing/signature.rs
@@ -34,3 +34,38 @@ pub fn signature(
     mac.update(string_to_sign.as_bytes());
     hex::encode(mac.finalize().into_bytes())
 }
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+    use time::PrimitiveDateTime;
+
+    use super::*;
+
+    #[test]
+    fn aws_example() {
+        let date = PrimitiveDateTime::parse(
+            "Fri, 24 May 2013 00:00:00 GMT",
+            "%a, %d %b %Y %-H:%M:%S GMT",
+        )
+        .unwrap();
+
+        let secret = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+        let region = "us-east-1";
+
+        let expected = "aeeed9bbccd4d02ee5c0109b86d86835f995330da4c265957d157751f604d404";
+
+        let got = signature(&date, secret, region, create_string_to_sign());
+
+        assert_eq!(got, expected);
+    }
+
+    fn create_string_to_sign() -> &'static str {
+        concat!(
+            "AWS4-HMAC-SHA256\n",
+            "20130524T000000Z\n",
+            "20130524/us-east-1/s3/aws4_request\n",
+            "3bfa292879f6447bbcda7001decf97f4a54dc650c8942174ae0a9121cf58ad04"
+        )
+    }
+}
