34889c92cd669afd4cea6a796a83a543bf982347
Paolo Barbolini <paolo@paolo565.org>
2020-11-27 17:43:21 +0100

    signing: add tests for string_to_string
    Uses the example from: https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-query-string-auth.html#query-string-auth-v4-signing-example
    
    Signed-off-by: Paolo Barbolini <paolo@paolo565.org>
    Co-authored-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/signing/string_to_sign.rs
+++ src/signing/string_to_sign.rs
@@ -13,9 +13,44 @@ pub fn string_to_sign(date: &PrimitiveDateTime, region: &str, canonical_request:
     format!("AWS4-HMAC-SHA256\n{}\n{}\n{}", iso8601, scope, hash)
 }
 
-mod test {
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+    use time::PrimitiveDateTime;
+
     use super::*;
 
     #[test]
-    fn test1() {}
+    fn aws_example() {
+        let date = PrimitiveDateTime::parse(
+            "Fri, 24 May 2013 00:00:00 GMT",
+            "%a, %d %b %Y %-H:%M:%S GMT",
+        )
+        .unwrap();
+
+        let region = "us-east-1";
+
+        let expected = concat!(
+            "AWS4-HMAC-SHA256\n",
+            "20130524T000000Z\n",
+            "20130524/us-east-1/s3/aws4_request\n",
+            "3bfa292879f6447bbcda7001decf97f4a54dc650c8942174ae0a9121cf58ad04"
+        );
+
+        let got = string_to_sign(&date, region, create_canonical_request());
+
+        assert_eq!(got, expected);
+    }
+
+    fn create_canonical_request() -> &'static str {
+        concat!(
+            "GET\n",
+            "/test.txt\n",
+            "X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIOSFODNN7EXAMPLE%2F20130524%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20130524T000000Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host\n",
+            "host:examplebucket.s3.amazonaws.com\n",
+            "\n",
+            "host\n",
+            "UNSIGNED-PAYLOAD",
+        )
+    }
 }
