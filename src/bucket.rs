159fd7be5feb3feb2ef386d594853a7677b4123c
Paolo Barbolini <paolo@paolo565.org>
2020-11-29 15:53:55 +0100

    bucket: generate the base_url when Bucket gets built
    Signed-off-by: Paolo Barbolini <paolo@paolo565.org>
    Co-authored-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/bucket.rs
+++ src/bucket.rs
@@ -3,13 +3,13 @@ use url::{ParseError, Url};
 use crate::signing::util::percent_encode_path;
 
 pub struct Bucket {
-    endpoint: Url,
+    base_url: Url,
     name: String,
     region: String,
 }
 
 impl Bucket {
-    pub fn new(endpoint: Url, name: String, region: String) -> Option<Self> {
+    pub fn new(endpoint: Url, path_style: bool, name: String, region: String) -> Option<Self> {
         let _ = endpoint.host_str()?;
 
         match endpoint.scheme() {
@@ -17,15 +17,17 @@ impl Bucket {
             _ => return None,
         };
 
+        let base_url = base_url(endpoint, &name, path_style);
+
         Some(Self {
-            endpoint,
+            base_url,
             name,
             region,
         })
     }
 
-    pub fn endpoint(&self) -> &Url {
-        &self.endpoint
+    pub fn base_url(&self) -> &Url {
+        &self.base_url
     }
 
     pub fn name(&self) -> &str {
@@ -36,25 +38,20 @@ impl Bucket {
         &self.region
     }
 
-    fn base_url(&self, path_style: bool) -> Url {
-        if path_style {
-            let path = format!("{}/", self.name);
-            self.endpoint.join(&path).unwrap()
-        } else {
-            let mut url = self.endpoint.clone();
-
-            let host = format!("{}.{}", self.name, url.host_str().unwrap());
-            url.set_host(Some(&host)).unwrap();
-
-            url
-        }
-    }
-
-    pub fn object_url(&self, path_style: bool, object: &str) -> Result<Url, ParseError> {
-        let base_url = self.base_url(path_style);
-
+    pub fn object_url(&self, object: &str) -> Result<Url, ParseError> {
         let object = percent_encode_path(object);
-        base_url.join(&object)
+        self.base_url.join(&object)
+    }
+}
+
+fn base_url(mut endpoint: Url, name: &str, path_style: bool) -> Url {
+    if path_style {
+        let path = format!("{}/", name);
+        endpoint.join(&path).unwrap()
+    } else {
+        let host = format!("{}.{}", name, endpoint.host_str().unwrap());
+        endpoint.set_host(Some(&host)).unwrap();
+        endpoint
     }
 }
 
@@ -63,13 +60,31 @@ mod tests {
     use super::*;
 
     #[test]
-    fn new() {
+    fn new_pathstyle() {
         let endpoint: Url = "https://s3.eu-west-1.amazonaws.com".parse().unwrap();
+        let base_url: Url = "https://s3.eu-west-1.amazonaws.com/rusty-s3/"
+            .parse()
+            .unwrap();
         let name = "rusty-s3";
         let region = "eu-west-1";
-        let bucket = Bucket::new(endpoint.clone(), name.into(), region.into()).unwrap();
+        let bucket = Bucket::new(endpoint.clone(), true, name.into(), region.into()).unwrap();
 
-        assert_eq!(bucket.endpoint(), &endpoint);
+        assert_eq!(bucket.base_url(), &base_url);
+        assert_eq!(bucket.name(), name);
+        assert_eq!(bucket.region(), region);
+    }
+
+    #[test]
+    fn new_domainstyle() {
+        let endpoint: Url = "https://s3.eu-west-1.amazonaws.com".parse().unwrap();
+        let base_url: Url = "https://rusty-s3.s3.eu-west-1.amazonaws.com"
+            .parse()
+            .unwrap();
+        let name = "rusty-s3";
+        let region = "eu-west-1";
+        let bucket = Bucket::new(endpoint.clone(), false, name.into(), region.into()).unwrap();
+
+        assert_eq!(bucket.base_url(), &base_url);
         assert_eq!(bucket.name(), name);
         assert_eq!(bucket.region(), region);
     }
@@ -79,22 +94,31 @@ mod tests {
         let endpoint = "file:///home/something".parse().unwrap();
         let name = "rusty-s3";
         let region = "eu-west-1";
-        assert!(Bucket::new(endpoint, name.into(), region.into()).is_none());
+        assert!(Bucket::new(endpoint, true, name.into(), region.into()).is_none());
     }
 
     #[test]
-    fn object_url() {
+    fn object_url_pathstyle() {
         let endpoint: Url = "https://s3.eu-west-1.amazonaws.com".parse().unwrap();
         let name = "rusty-s3";
         let region = "eu-west-1";
-        let bucket = Bucket::new(endpoint.clone(), name.into(), region.into()).unwrap();
+        let bucket = Bucket::new(endpoint.clone(), true, name.into(), region.into()).unwrap();
 
-        let path_style = bucket.object_url(true, "something/cat.jpg").unwrap();
-        let domain_style = bucket.object_url(false, "something/cat.jpg").unwrap();
+        let path_style = bucket.object_url("something/cat.jpg").unwrap();
         assert_eq!(
             "https://s3.eu-west-1.amazonaws.com/rusty-s3/something/cat.jpg",
             path_style.as_str()
         );
+    }
+
+    #[test]
+    fn object_url_domainstyle() {
+        let endpoint: Url = "https://s3.eu-west-1.amazonaws.com".parse().unwrap();
+        let name = "rusty-s3";
+        let region = "eu-west-1";
+        let bucket = Bucket::new(endpoint.clone(), false, name.into(), region.into()).unwrap();
+
+        let domain_style = bucket.object_url("something/cat.jpg").unwrap();
         assert_eq!(
             "https://rusty-s3.s3.eu-west-1.amazonaws.com/something/cat.jpg",
             domain_style.as_str()
