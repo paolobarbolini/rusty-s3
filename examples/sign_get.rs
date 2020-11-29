5d99b4b6466f59490e806d60a7d3cf19dabfa33d
Paolo Barbolini <paolo@paolo565.org>
2020-11-29 16:37:27 +0100

    examples: update sign_get example to use the higher level APIs
    Signed-off-by: Paolo Barbolini <paolo@paolo565.org>
    Co-authored-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- examples/sign_get.rs
+++ examples/sign_get.rs
@@ -1,15 +1,21 @@
-use rusty_s3::sign;
-use time::OffsetDateTime;
+use std::time::Duration;
+
+use rusty_s3::actions::{GetObject, S3Action};
+use rusty_s3::{Bucket, Credentials};
+
+const ONE_HOUR: Duration = Duration::from_secs(3600);
 
 fn main() {
-    let date = OffsetDateTime::now_utc();
-
-    let url = "http://localhost:9000/test/img.jpg".parse().unwrap();
+    let url = "http://localhost:9000".parse().unwrap();
     let key = "minioadmin";
     let secret = "minioadmin";
     let region = "minio";
-    let expires_seconds = 3600;
 
-    let signed = sign(&date, "GET", &url, key, secret, region, expires_seconds);
-    println!("signed: {}", signed);
+    let bucket = Bucket::new(url, true, "test".into(), region.into()).unwrap();
+    let credential = Credentials::new(key.into(), secret.into());
+
+    let get_obj = GetObject::new(&bucket, Some(&credential), "img.jpg");
+    let url_generated = get_obj.sign(ONE_HOUR);
+
+    println!("url: {}", url_generated);
 }
