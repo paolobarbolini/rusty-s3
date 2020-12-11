966053b1c6068eb41699f7bb5eab9639942786c6
Paolo Barbolini <paolo@paolo565.org>
2020-12-11 17:54:44 +0100

    method: improve coverage
    Signed-off-by: Paolo Barbolini <paolo@paolo565.org>
    Co-authored-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/method.rs
+++ src/method.rs
@@ -28,3 +28,26 @@ impl Display for Method {
         f.write_str(self.to_str())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn to_str() {
+        assert_eq!(Method::Head.to_str(), "HEAD");
+        assert_eq!(Method::Get.to_str(), "GET");
+        assert_eq!(Method::Post.to_str(), "POST");
+        assert_eq!(Method::Put.to_str(), "PUT");
+        assert_eq!(Method::Delete.to_str(), "DELETE");
+    }
+
+    #[test]
+    fn display() {
+        assert_eq!(Method::Head.to_string(), "HEAD");
+        assert_eq!(Method::Get.to_string(), "GET");
+        assert_eq!(Method::Post.to_string(), "POST");
+        assert_eq!(Method::Put.to_string(), "PUT");
+        assert_eq!(Method::Delete.to_string(), "DELETE");
+    }
+}
