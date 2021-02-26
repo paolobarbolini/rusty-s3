9fd1f6729d5db38605598326fb63b0212e60034c
Paolo Barbolini <paolo@paolo565.org>
2021-02-26 21:25:27 +0100

    tests: add minio test for list_objects_v2
    Signed-off-by: Paolo Barbolini <paolo@paolo565.org>
    Co-authored-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- tests/upload_download.rs
+++ tests/upload_download.rs
@@ -1,12 +1,31 @@
 use std::time::Duration;
 
-use rusty_s3::actions::S3Action;
+use rusty_s3::actions::{ListObjectsV2, S3Action};
 
 mod common;
 
 #[tokio::test]
 async fn test1() {
     let (bucket, credentials, client) = common::bucket().await;
+
+    let action = bucket.list_objects_v2(Some(&credentials));
+    let url = action.sign(Duration::from_secs(60));
+    let resp = client
+        .get(url)
+        .send()
+        .await
+        .expect("send ListObjectsV2")
+        .error_for_status()
+        .expect("ListObjectsV2 unexpected status code");
+    let text = resp.text().await.expect("ListObjectsV2 read respose body");
+    let list = ListObjectsV2::parse_response(&text).expect("ListObjectsV2 parse response");
+
+    assert_eq!(list.contents.is_empty(), true);
+
+    assert_eq!(list.max_keys, 4500);
+    assert!(list.common_prefixes.is_empty());
+    assert!(list.next_continuation_token.is_none());
+    assert!(list.start_after.is_none());
 
     let body = vec![b'r'; 1024];
 
