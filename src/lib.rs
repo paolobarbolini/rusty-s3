ef8055bd776a73c8af8531bde2f6f87897893a14
Paolo Barbolini <paolo@paolo565.org>
2021-11-04 19:28:11 +0100

    Prepare 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -40,7 +40,7 @@
     rustdoc::broken_intra_doc_links
 )]
 #![forbid(unsafe_code)]
-#![doc(html_root_url = "https://docs.rs/rusty-s3/0.1.2")]
+#![doc(html_root_url = "https://docs.rs/rusty-s3/0.2.0")]
 
 pub use self::actions::S3Action;
 pub use self::bucket::{Bucket, UrlStyle};
