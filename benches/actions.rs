bcb28a63d7b35877fe74efa631bad780cd9fa8fc
Paolo Barbolini <paolo.barbolini@m4ss.net>
2025-07-20 11:02:54 +0200

    build(deps): upgrade criterion to v0.6

--- benches/actions.rs
+++ benches/actions.rs
@@ -1,6 +1,6 @@
-use std::time::Duration;
+use std::{hint::black_box, time::Duration};
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 
 use rusty_s3::{Bucket, Credentials, S3Action as _, UrlStyle};
 
